f82bbca511ae30b04b688cf51ab7848f89b0d564
DarshSaxena <161448152+DarshSaxena@users.noreply.github.com>
2025-07-12 22:41:43 +0530

    Add files via upload

---
+++ practice set2.py
@@ -0,0 +1,33 @@
+'''
+a = 5 
+b = 34
+print(a+b)
+'''
+'''
+a=30
+z=5
+print(a/z)
+'''
+'''
+a=int(input("enter the value of a: "))
+t=type(a)
+print(t)
+'''
+a=34
+b=67
+print(b>a)
+'''
+'''
+'''
+a=int(input("enter a number" ))
+b=int(input("enter a number"))
+avg=a+b/2
+print(avg)
+t=type(avg)
+print(t)
+'''
+'''
+a=int(input("enter a number 6"))
+b=a*a
+print(b)
+'''
