3512a39dc21f243b07e0aaa678e2a540f22c4b7f
DarshSaxena <161448152+DarshSaxena@users.noreply.github.com>
2025-07-13 23:59:08 +0530

    Add files via upload

---
+++ practice set3.py
@@ -0,0 +1,24 @@
+'''
+a=input("enter the name: ")
+print(f"good afternoon {a}")#used f string to print on the same line
+'''
+'''
+letter =''''''Dear <|Name|>,
+You are selected!
+<|Date|>'''''''''
+#print(letter.replace("<|Name|>","Darsh").replace("<|Date|>","24th october,2025")) #replace funtion to fill the blanks
+'''
+name="darsh is so handsome"
+print(name.find(" "))
+'''
+'''
+'''
+name="darsh is so handsome"
+print(name.replace(" ","gh"))
+'''
+'''
+'''
+''''''''''''
+name="dear darsh,\t\nthank you for everything.\n!thanks"
+print(name)
+'''
