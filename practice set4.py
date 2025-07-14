7beed569f84cb19402379ea3b233b13937612599
DarshSaxena <161448152+DarshSaxena@users.noreply.github.com>
2025-07-14 20:42:47 +0530

    Add files via upload

---
+++ practice set4.py
@@ -0,0 +1,53 @@
+'''
+fruits=[]
+f1=input("enter first name :")
+fruits.append(f1)
+f2=input("enter first name :")
+fruits.append(f2)
+f3=input("enter first name :")
+fruits.append(f3)
+f4=input("enter first name :")#append is a function used for entering a value 
+fruits.append(f4)
+f5=input("enter first name :")
+fruits.append(f5)
+f6=input("enter first name :")
+fruits.append(f6)
+f7=input("enter first name :")
+fruits.append(f7)
+print(fruits)
+'''
+'''
+marks=[]
+print(type(marks))
+m1=input("enter marks of 1:")
+marks.append(m1)
+m2=input("enter marks :")
+marks.append(m2)
+m3=input("enter marks :")
+marks.append(m3)
+m4=input("enter marks :")
+marks.append(m4)
+marks.sort()#sort function for sorting a value
+print(marks)
+'''
+'''
+'''
+a=()
+a= 45
+a=[]
+value=type(a)
+print(value)
+'''
+'''
+'''
+list=[1,2,3,4]
+value=sum(list)#sum value
+print(value)
+'''
+'''
+'''
+'''
+list=[1,0,9,8,0,7,0]
+value= list.count(0)#count the occurence of any element 
+print(value)
+'''
