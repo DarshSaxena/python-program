2e22809d3577460079d9056ae708b9fb1e00630d
DarshSaxena <161448152+DarshSaxena@users.noreply.github.com>
2025-07-15 22:31:56 +0530

    Add files via upload

---
+++ practice set5.py
@@ -0,0 +1,82 @@
+'''
+hinglish={"FOOD":"KHNNA","POTATO":"AALO","HINDI":"ENGLISH","INDIA":"BHARAT" }
+print(hinglish,type(hinglish))
+word=input("enter the food word")
+
+print(hinglish[word])
+'''
+'''
+s = set()
+s1=int(input("enter the  number"))
+s.add(s1)
+s2=int(input("enter the number"))
+s.add(s2)
+s3=int(input("enter the  number"))
+s.add(s3)
+s4=int(input("enter the number"))
+s.add(s4)
+s5=int(input("enter the number"))
+s.add(s5)
+s6=int(input("enter the  number"))
+s.add(s6)
+s7=int(input("enter the number"))
+s.add(s7)
+s8=int(input("enter the number"))
+s.add(s8)
+print (s)
+'''
+
+
+'''
+s={18,"18"}
+print(s,type(s))
+'''
+'''
+d={}
+name=input("enter your name")
+lang=input("enter your lang")
+d.update({name:lang})
+name1=input("enter your name")
+lang1=input("enter your lang")
+d.update({name1:lang1})
+name2=input("enter your name")
+lang2=input("enter your lang")
+d.update({name2:lang2})
+name3=input("enter your name")
+lang3=input("enter your lang")
+d.update({name3:lang3})
+
+print(d)
+'''
+'''
+d={}
+name=input("enter your name")
+lang=input("enter your lang")
+d.update({name:lang})
+name1=input("enter your name")
+lang1=input("enter your lang")
+d.update({name1:lang1})
+name2=input("enter your name")
+lang2=input("enter your lang")
+d.update({name2:lang2})
+name3=input("enter your name")
+lang3=input("enter your lang")
+d.update({name3:lang3})
+
+print(d)
+'''
+d={}
+name=input("enter your name")
+lang=input("enter your lang")
+d.update({name:lang})
+name1=input("enter your name")
+lang1=input("enter your lang")
+d.update({name1:lang1})
+name2=input("enter your name")
+lang2=input("enter your lang")
+d.update({name2:lang2})
+name3=input("enter your name")
+lang3=input("enter your lang")
+d.update({name3:lang3})
+
+print(d)
