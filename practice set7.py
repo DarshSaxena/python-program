fe45db3e84be674961876cc308f891b797b48495
DarshSaxena <161448152+DarshSaxena@users.noreply.github.com>
2025-07-17 23:09:04 +0530

    Add files via upload

---
+++ practice set7.py
@@ -0,0 +1,48 @@
+'''
+a=int(input("enter the number you want to print table"))
+for i in range(10):
+i+=1
+c=i*a
+print(c)
+'''
+'''
+l=["ROHAN","ROHIT","SHREYASH","SHREYA","SHEELA"]
+for name in l:
+if(name.startswith("S")):
+print("hello",name)
+'''
+'''
+a=int(input("enter the number you want to print table"))
+i=0
+while(i<10):
+i+=1
+c=i*a
+print(c)
+'''
+'''
+n=int(input("enter the length :"))
+i=1
+c=0
+while(i<=n):
+c=c+1
+i+=1 
+print(c)
+'''
+'''
+n =int(input("enter the length :"))
+i=1
+c=1
+while(i<=n):
+c=c*i
+i+=1 
+print(c)
+'''
+'''
+n=int(input("enter the tree limit :"))
+i=0
+j=0
+while(i<=n):
+while(j<=i):
+j+=24
+print("*")
+'''
