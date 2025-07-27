ca742d8ba5a62fe44179dba8adb80243c5964093
Darsh Saxena <161448152+DarshSaxena@users.noreply.github.com>
2025-07-28 03:49:14 +0530

    Add files via upload

---
+++ practice set 10.py
@@ -0,0 +1,34 @@
+'''
+class programmer:
+    salary=12345454
+    age=45
+    dept="socs"
+def getinfo(self):
+    print("salary="+str(self.salary),"age="+str(self.age),"department="+str(self.dept))
+darsh=programmer()
+darsh.name="DARSH"
+print(darsh.name)
+getinfo(darsh)
+'''
+import math
+
+choice=input("enter your choice c for square,cc for cube and ccc for square root:")
+print(choice)
+class calculator:
+    def square(self,a):
+     return a*a
+     
+    def cube(self,a):
+     return a*a*a
+    def squareroot(self,a):
+     return math.sqrt(a)
+darsh=calculator()
+a=float(input("enter the number"))
+if(choice=="c"):
+    print(darsh.square(a))
+elif(choice=="cc"):
+    print(darsh.cube(a))
+elif(choice=="ccc"):
+    print(darsh.squareroot(a))
+else:
+    print("invalid choice")
