9c541f4edb28e3f20e5430d0e13b9e763ba76199
Darsh Saxena <161448152+DarshSaxena@users.noreply.github.com>
2025-10-29 04:22:46 +0530

    array creation.py

---
+++ array creation.py
@@ -0,0 +1,25 @@
+'''
+import numpy as np
+'''
+'''
+a = np.array([[1,2],
+           [2,4],
+           [3,5]])
+print(a)
+'''
+'''
+b=np.zeros([3,2])
+print(b)
+'''
+'''
+c=np.ones([9,9])
+print(c)
+'''
+'''
+d =np.random.randint(3,3)
+print(d)
+'''
+'''
+e=np.random.randint(low=0,high=4,size=(3,3))
+print(e)
+'''
